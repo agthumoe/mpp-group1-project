52a04c98e65a9d26e405868873d9c8bc98ae5fee
Zaw Ye Naing <apple.zawyenaing@gmail.com>
2024-09-14 00:55:17 -0500

    modify UI for members.

--- src/ui/AllMembersWindow.java
+++ src/ui/AllMembersWindow.java
@@ -9,6 +9,7 @@ import ui.components.ImmutableTableModel;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.util.List;
@@ -44,10 +45,15 @@ public class AllMembersWindow extends MenusWindow {
     @Override
     public void formatContentPane() {
         this.setTitle("Library Members");
+        Font headerFont = new Font("Arial", Font.BOLD, 14);
         getContentPane().setLayout(new BorderLayout(0, 0));
         String[] columnNames = {"Member ID", "First Name", "Last Name", "Street", "City", "State", "Zip", "Telephone"};
         this.tableModel = new ImmutableTableModel(columnNames, 0);
         this.table = new JTable(this.tableModel);
+        this.table.setRowHeight(40);
+        this.table.getTableHeader().setFont(headerFont);
+        this.table.setDefaultRenderer(Object.class, new GradientTableCellRenderer());
+
         JScrollPane scrollPanel = new JScrollPane(table);
         scrollPanel.setBorder(new EmptyBorder(0, 10, 10, 10));
         getContentPane().add(scrollPanel);
@@ -78,5 +84,36 @@ public class AllMembersWindow extends MenusWindow {
             String[] row = new String[]{member.getMemberId(), member.getFirstName(), member.getLastName(), member.getAddress().getStreet(), member.getAddress().getCity(), member.getAddress().getState(), member.getAddress().getZip(), member.getTelephone()};
             this.tableModel.addRow(row);
         }
+    }
+}
+
+
+class GradientTableCellRenderer extends DefaultTableCellRenderer {
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+        if (!isSelected && row % 2 == 0) { // Apply gradient to every other row
+            setBackground(new Color(0, 0, 0, 0)); // Transparent background to allow gradient
+        } else {
+            setBackground(table.getBackground());
+        }
+
+        return c;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if (getBackground().getAlpha() == 0) { // Only paint gradient if background is transparent
+            Graphics2D g2d = (Graphics2D) g;
+            int width = getWidth();
+            int height = getHeight();
+            Color startColor = Color.lightGray;
+            Color endColor = Color.lightGray;
+            GradientPaint gradientPaint = new GradientPaint(0, 0, startColor, 0, height, endColor);
+            g2d.setPaint(gradientPaint);
+            g2d.fillRect(0, 0, width, height);
+        }
+        super.paintComponent(g);
     }
 }
